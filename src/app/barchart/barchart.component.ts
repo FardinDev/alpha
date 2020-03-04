3a5661ce098daaa4f9c7cf9434b79649c533320f
Fardin <phpfardin@gmail.com>
2020-03-04 17:33:06 +0600

    no cmt\

--- src/app/barchart/barchart.component.ts
+++ src/app/barchart/barchart.component.ts
@@ -16,11 +16,11 @@ export class BarchartComponent implements OnInit {
   barChartLabels: Label[] = ['Apple', 'Banana', 'Kiwifruit', 'Blueberry', 'Orange', 'Grapes'];
   barChartType: ChartType = 'bar';
   barChartLegend = true;
-  barChartPlugins = [];
+  barChartPlugins = []; 
 
   barChartData: ChartDataSets[] = [
     { data: [45, 37, 60, 70, 46, 33], label: 'Best Fruits', backgroundColor: 'rgb(115,151,115)', hoverBackgroundColor:'rgb(80,151,115)' },
-    { data: [45, 37, 40, 55, 46, 55], label: 'Best Flowers', backgroundColor: 'rgb(115,0,11)', hoverBackgroundColor: 'rgb(80,0,11)'}
+    { data: [11, 50, 40, 55, 70, 30], label: 'Best Flowers', backgroundColor: 'rgb(115,0,11)', hoverBackgroundColor: 'rgb(80,0,11)'}
   ];
 
 
